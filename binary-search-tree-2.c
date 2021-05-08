0b9405d805fc6b2f7333b51412a1113a729e0eaf
sangholee990 <80809068+sangholee990@users.noreply.github.com>
2021-05-09 01:44:02 +0900

    last commit

--- binary-search-tree-2.c
+++ binary-search-tree-2.c
@@ -17,7 +17,7 @@ typedef struct node {
 	int key;
 	struct node *left;
 	struct node *right;
-} Node;
+} Node; //트리구성노드  
 
 /* for stack */
 #define MAX_STACK_SIZE		20
@@ -50,7 +50,8 @@ int freeBST(Node* head); /* free all memories allocated to the tree */
 /* you may add your own defined functions if necessary */
 
 
-void printStack();
+int is_Empty(); //비었는지 검사하는 함수 
+int is_Full(); // 꽉찼는지 검사하는 함수 
 
 
 
@@ -105,10 +106,6 @@ int main()
 			levelOrder(head->left);
 			break;
 
-		case 'p': case 'P':
-			printStack();
-			break;
-
 		default:
 			printf("\n       >>>>>   Concentration!!   <<<<<     \n");
 			break;
@@ -140,7 +137,7 @@ int initializeBST(Node** h) {
 
 
 
-void recursiveInorder(Node* ptr)
+void recursiveInorder(Node* ptr) //인오더 순회 함수 
 {
 	if(ptr) {
 		recursiveInorder(ptr->left);
@@ -152,22 +149,73 @@ void recursiveInorder(Node* ptr)
 /**
  * textbook: p 224s
  */
-void iterativeInorder(Node* node)
+void iterativeInorder(Node* node) //재귀 호출 사용하지 않는 인오더 
 {
+	if (node ==NULL){ //tree에 노드가 없을 경우 
+		 printf("트리에 노드가 없습니다. \n");
+	}
+	
+	while(1) //무한 루프 
+	{
+		for(;node;node=node->left) // 제일 왼쪽 노드까지 루프 
+		   push(node); //스택에 하나씩 삽입
+		
+		node = pop(); //스택에서 하나 팝한다 
+		
+		if(!node) // 스택이 공백일경우
+		    break; //무한 루프 탈출
+			
+		printf("[%d]",node->key); // 팝한노드 하나씩 출력
+		 	 
+		node=node->right; //노드 의 오른쪽으로 이동   
+	}
+	
 }
 
 /**
  * textbook: p 225
  */
-void levelOrder(Node* ptr)
+void levelOrder(Node* ptr) //레벨 순서 순회 
 {
+	
+	  int i; 
+	  front= -1; 
+	  rear = -1; //  초기값 설정 
+	  
+	  for(i=0;i<MAX_QUEUE_SIZE-1;i++)
+	   {
+	  	  queue[i] = NULL;  
+	   }
+	  
+	  if(ptr==NULL) //공백트리면 
+	      return;// 함수 종료 
+	  
+	  enQueue(ptr); //큐에 노드하나 삽입
+	  
+	  while(1)
+	  {
+	  	 ptr = deQueue(); //프론트에 값 삭제
+		   
+		 if(ptr) // 삭제할 값이 남아있다면 
+		{
+		 	 printf("[%d]",ptr->key); // 디큐한 노드 출력 
+		 	 if(ptr->left) // 왼쪽 자식 노드가 존재할시
+			     enQueue(ptr->left);//왼쪽 자식 노드를 큐에 넣는다
+			 if(ptr->right) // 오른쪽 자식 노드가 존재할 시	 
+			     enQueue(ptr->right); //오른쪽 자식 노드를 큐에 넣는다 
+	  	}   
+	    
+		else break;  // 삭제할 값이 없으면 탈출 
+      } 	     
 }
 
 
 int insert(Node* head, int key)
 {
+	//뉴노드 동적할당 
 	Node* newNode = (Node*)malloc(sizeof(Node));
 	newNode->key = key;
+	//리프노드 초기화 
 	newNode->left = NULL;
 	newNode->right = NULL;
 
@@ -178,8 +226,8 @@ int insert(Node* head, int key)
 
 	/* head->left is the root */
 	Node* ptr = head->left;
-
 	Node* parentNode = NULL;
+	
 	while(ptr != NULL) {
 
 		/* if there is a node for the key, then just return */
@@ -210,15 +258,15 @@ int insert(Node* head, int key)
 
 int deleteNode(Node* head, int key)
 {
-	Node* lead =NULL;
+	Node* lead = head->left; //루트노드 초기화 
 	Node* pre=NULL; //바꿀 노드 위치 저장 
+	Node* temp=NULL; //임시저장 포인터
+	 
+
 	
-	Node* temp=NULL;
-	
-	lead = head->left; // 루트노드 
-	
-	if(lead=NULL){ //트리에 노드가 존재하지 않으면
+	if(lead==NULL){ //트리에 노드가 존재하지 않으면
 	    printf("이 노드는 트리에 없습니다.\n");
+	    return 0;
 	}
 	
 	while(lead) //트리에 노드가 하나 이상일 때 
@@ -388,7 +436,7 @@ int deleteNode(Node* head, int key)
 
 void freeNode(Node* ptr)
 {
-	if(ptr) {
+	if(ptr) { //포스트오더 방식으로 트리의 노드 동적 할당 해제 
 		freeNode(ptr->left);
 		freeNode(ptr->right);
 		free(ptr);
@@ -398,17 +446,17 @@ void freeNode(Node* ptr)
 int freeBST(Node* head)
 {
 
-	if(head->left == head)
+	if(head->left == head) //트리에 노드가 없다면 
 	{
 		free(head);
 		return 1;
 	}
 
-	Node* p = head->left;
+	Node* p = head->left; //루트노드 위치 저장 
 
-	freeNode(p);
+	freeNode(p); // 노드 할당 해제 
 
-	free(head);
+	free(head); //헤드노드 메모리 해제 
 	return 1;
 }
 
@@ -416,18 +464,63 @@ int freeBST(Node* head)
 
 Node* pop()
 {
+	if(top == -1) //스택이 비었을 경우 
+	   return 0; // 함수 종료
+	else //스택이 비어있지 않다면
+	  return stack[top--]; //pop    
 }
 
 void push(Node* aNode)
 {
+	if(top == MAX_STACK_SIZE-1) //스택이 가득 차있을 경우
+	   return ; //함수 종료
+	else 
+	   stack[++top]=aNode; //탑 증가후 값 저장 
 }
 
 
 
 Node* deQueue()
 {
+	 if(is_Empty()==1) //큐가 비어있을 경우
+	     return NULL; 
+	     
+	 front=(front+1)%MAX_QUEUE_SIZE; //원형큐 인덱스 증가
+	 return queue[front]; // 제거노드 리턴    
 }
 
 void enQueue(Node* aNode)
 {
+	rear=(rear+1)%MAX_QUEUE_SIZE; //원형큐 꼬리 인덱스 증가
+	
+	if(is_Full()==1) //가득차있다면
+	   return; //함수종료
+	
+	queue[rear]=aNode; //가득 차있지 않으면 꼬리에 노드 삽입    
+}
+
+int is_Empty() 
+{
+	if(front == rear) //front와 rear가 같으면 원소가 비어있는 것이다
+	{
+		return 1; //원소가 비어있을 때는 1을 리턴한다
+	}
+	return 0;
+}
+
+int is_Full() 
+{
+	if(front == rear) //full 상태인경우 
+	{
+		if(rear == 0) 
+		{
+			rear = MAX_QUEUE_SIZE-1;
+		}
+		else
+		{
+			rear = rear-1;
+		}
+		return 1; //큐가 가득찼다면 함수종료 
+	}
+	return 0;
 }
